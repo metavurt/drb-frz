1fc93bfce2345fd0698857ec803a764f8ccc3ac9
metavurt <billy.oneal@tahoepartners.com>
2014-05-10 06:30:18 -0500

    make lots changes

---
+++ js/drbfz.js
@@ -0,0 +1,15 @@
+var teamGameTotal = teamScratch + teamHandicap;
+var playerScratch = points;
+var playerHandicap = masterHandicap - (totalScratch / totalGames) * percentDifferenceSomething;
+var teamPinTotal = totalCountHandicapCounts
+
+
+
+function calcTeamGameTotal(tid, wid, gnum) {
+	/* teamID, weekID, gameNum */
+
+	// get all players with teamID = tid from weekID = wid, for game number = gnum
+
+
+
+}
---
+++ php/zj.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_NAME', 'weo3_drb');
+define('DB_USER', 'weo3_weo3');
+define('DB_PASSWORD', 'p4ntaLoc1ni');
+define('DB_HOST', 'localhost');
+
+
+?>
