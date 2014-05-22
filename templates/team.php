2403596fcb2730b0ca2b8a41e6fc7da389360b1d
metavurt <billy.oneal@tahoepartners.com>
2014-05-22 16:48:33 -0500

    oracle dot com would be amazing

--- templates/team.php
+++ templates/team.php
@@ -146,15 +146,12 @@ $(function () {
         },
         plotOptions: {
             pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: false
+				},
+				showInLegend: true
             }
         },
         credits: {
