cc3113cc92a18baa24053afc28b594c9dd9ea7cf
metavurt <billy.oneal@tahoepartners.com>
2014-05-29 10:08:49 -0500

    whats up

--- js/asdf.sql
+++ js/asdf.sql
@@ -3,16 +3,3 @@ update bnp_points set tpins='2144' where tid='1' and wid='6';
 
 
 
-
-('SELECT tname, bnp_players.pid as pid, pname, g1, g2, g3,
-					SUM(g1 + g2 + g3) as tp,
-                    ROUND( SUM(g1 + g2 + g3) + SUM(hnd)/COUNT(wid), 0) as tph,
-					ROUND(SUM(g1 + g2 + g3)/(COUNT(wid)*3), 0) as avrg,
-					ROUND(SUM(hnd)/COUNT(wid), 0) as h
-					FROM bnp_teams, bnp_players
-					JOIN bnp_stats
-					ON bnp_stats.pid = bnp_players.pid
-					WHERE bnp_teams.tid = '.$team.' AND bnp_players.tid = '.$team.'
-					AND bnp_stats.wid = '.$wk.'
-					GROUP BY pid');
-
